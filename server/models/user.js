00eb632e073bad274aa03500f8ac8c5275070ea4
Alliance Peace <peace@sda.tres.rw>
2018-08-17 11:57:00 +0200

    add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -46,6 +46,25 @@ UserSchema.methods.generateAuthToken = function () {
 
     return user.save().then(() => token);
 };
+UserSchema.statics.findByToken = function (token) {
+    var User = this;
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'access');
+    } catch(e) {
+        // return new Promise(resolve, reject) => {
+        //     reject();
+        // };
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+};
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
